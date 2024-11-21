834c8973c84f23caae14dbb6bf128673ff8b0d63
Yuliya Bondarenko <juliyaodessa84@gmail.com>
2024-11-21 22:31:59 +0100

    1280

--- main.js
+++ main.js
@@ -34,3 +34,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
   handleScrollAnimation();
 });
+////////////////////////////
+// Tech Skils//
+function findCommon(arr1, arr2) {
+  const common = arr1.filter((num) => arr2.includes(num));
+
+  return common;
+}
+
+console.log(findCommon([7, 8, 9], [9, 10, 11])); // [9]
+console.log(findCommon([1, 2, 3], [2, 3, 4])); // [2, 3]
